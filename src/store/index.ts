df07d696c88048e5af507f46e03b73ad41914fc6
deacon <soydeacon@gmail.com>
2025-05-05 21:53:47 -0700

    Update branding with Roboto font and Wordly colors, fix Next.js config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import sessionReducer from './slices/sessionSlice';
-import transcriptReducer from './slices/transcriptSlice';
-import glossaryReducer from './slices/glossarySlice';
+
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    session: sessionReducer,
-    transcript: transcriptReducer,
-    glossary: glossaryReducer,
+
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
